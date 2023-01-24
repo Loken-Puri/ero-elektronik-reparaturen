29c2303895595a4eb250b1a7855632c59395347c
Lokendra Puri <lpuri1979@gmail.com>
2023-01-24 15:03:19 +0000

    change name

--- components/Layout.js
+++ components/Layout.js
@@ -50,7 +50,7 @@ export default function Layout({title, children}) {
             <header>
                 <nav className="flex h-12 items-center px-4 justify-between shadow-md">
                     <Link href="/" className='text-lg font-bold text-green-900 w-64'>
-                        ERO ELEKTRONIK
+                        ONLINE SHOPPING
                     </Link>
 
                     {/* ---------- Search box starts ----------*/}
