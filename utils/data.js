8676cdc2ce7d3b140b476b3a176ddef287727bf1
Lokendra Puri <lpuri1979@gmail.com>
2023-01-24 14:06:09 +0000

    remove env

--- utils/data.js
+++ utils/data.js
@@ -18,18 +18,18 @@ const data = {
     ],
     products: [
         {
-            name: 'Free Shirt',
-            slug: 'free-shirt',
-            category: 'Shirts',
-            image: '/images/shirt1.jpg',
-            price: 70,
-            brand: 'Nike',
+            name: 'Cuisinart',
+            slug: 'Cuisinart',
+            category: 'Coffee-Mechine',
+            image: '/images/Cuisinart-159.jpg',
+            price: 159,
+            brand: 'Cuisinart',
             rating: 4.5,
             numReviews: 8,
             countInStock: 20,
-            description: 'A popular shirt',
+            description: 'A popular Coffee Mechine',
             isFeatured: true,
-            banner: '/images/banner1.jpg',
+            banner: '/images/Cuisinart-159.jpg',
         },
         {
             name: 'Fit Shirt',
@@ -98,3 +98,111 @@ const data = {
 
 
 export default data;
+
+
+
+
+
+
+
+
+// import bcrypt from 'bcryptjs';
+
+// const data = {
+//     users: [
+//         {
+//             name: 'Loken',
+//             email: 'admin@ero.com',
+//             password: bcrypt.hashSync('123456'),
+//             isAdmin: true,
+//         },
+
+//         {
+//             name: 'Krishna',
+//             email: 'user@ero.com',
+//             password: bcrypt.hashSync('123456'),
+//             isAdmin: false,
+//         },
+//     ],
+//     products: [
+//         {
+//             name: 'Cuisinart',
+//             slug: 'Cuisinart',
+//             category: 'Coffee-Mechine',
+//             image: '/images/Cuisinart-159.jpg',
+//             price: 159,
+//             brand: 'Cuisinart',
+//             rating: 4.5,
+//             numReviews: 8,
+//             countInStock: 20,
+//             description: 'A popular Coffee Mechine',
+//             isFeatured: true,
+//             banner: '/images/Cuisinart-159.jpg',
+//         },
+//         {
+//             name: 'Keurig-coffee-mechine-155',
+//             slug: 'Keurig-coffee-mechine-155',
+//             category: 'Coffee Mechine',
+//             image: '/images/Keurig-coffee-mechine-155.jpg',
+//             price: 155,
+//             brand: 'Keurig',
+//             rating: 3.2,
+//             numReviews: 10,
+//             countInStock: 20,
+//             description: 'A popular Coffee Mechine',
+//             isFeatured: true,
+//             banner: '/images/Keurig-coffee-mechine-155.jpg',
+//         },
+//         {
+//             name: 'Lor-barista-189',
+//             slug: 'Lor-barista-189',
+//             category: 'Coffee Mechine',
+//             image: '/images/Lor-barista-189.jpg',
+//             price: 189,
+//             brand: 'LOR',
+//             rating: 4.5,
+//             numReviews: 3,
+//             countInStock: 20,
+//             description: 'A popular Coffee Mechine',
+//         },
+//         {
+//             name: 'Philips-espresso-mechine-799',
+//             slug: 'Philips-espresso-mechine-799',
+//             category: 'Coffee Mechine',
+//             image: '/images/Philips-espresso-mechine-799.jpg',
+//             price: 799,
+//             brand: 'Philips',
+//             rating: 2.9,
+//             numReviews: 13,
+//             countInStock: 20,
+//             description: 'A popular Coffee Mechine',
+//         },
+//         {
+//             name: 'Philips-latest-series-549',
+//             slug: 'Philips-latest-series-549',
+//             category: 'Coffee Mechine',
+//             image: '/images/Philips-latest-series-549.jpg',
+//             price: 549,
+//             brand: 'Philips',
+//             rating: 3.5,
+//             numReviews: 7,
+//             countInStock: 20,
+//             description: 'A popular Coffee Mechine',
+//         },
+//         {
+//             name: 'Spinn-coffee-maker-799',
+//             slug: 'Spinn-coffee-maker-799',
+//             category: 'Coffee Mechine',
+//             image: '/images/Spinn-coffee-maker-799.jpg',
+//             price: 75,
+//             brand: 'Spinn',
+//             rating: 2.4,
+//             numReviews: 14,
+//             countInStock: 20,
+//             description: 'A popular Coffee Mechine',
+//         },
+//         ],
+// };
+
+
+// export default data;
